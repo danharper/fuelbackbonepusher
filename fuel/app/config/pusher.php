a8a99d060ea111fecadf3e1c8c6658eae212918f
Dan Harper <intouch@danharper.me>
2012-05-07 12:44:04 +0100

    Always get Pusher keys from ENV

--- fuel/app/config/pusher.php
+++ fuel/app/config/pusher.php
@@ -4,7 +4,7 @@ return array(
 	'host'     => 'http://api.pusherapp.com',
 	'port'     => 80,
 	'timeout'  => 30,
-	'auth_key' => 'b54e55afad8e55e83760',
-	'secret'   => '4c34058d92312288727e',
-	'app_id'   => '19965',
+	'auth_key' => getenv('PUSHER_KEY'),
+	'secret'   => getenv('PUSHER_SECRET'),
+	'app_id'   => getenv('PUSHER_APP_ID'),
 );
