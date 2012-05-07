0eea151de4f2d080e9f21cc787656727fb157f92
Dan Harper <intouch@danharper.me>
2012-05-07 01:01:31 +0100

    update pusher api

--- fuel/app/config/pusher.php
+++ fuel/app/config/pusher.php
@@ -4,7 +4,7 @@ return array(
 	'host'     => 'http://api.pusherapp.com',
 	'port'     => 80,
 	'timeout'  => 30,
-	'auth_key' => 'a09459a9d691445e3e1f',
-	'secret'   => '6f1edffc7901de300b03',
+	'auth_key' => '3b36299ab399c2b65106',
+	'secret'   => '719660cf6f838cf4d425',
 	'app_id'   => '19965',
 );
